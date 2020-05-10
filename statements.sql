13e33d9d622239e3644f84db42fda7dc03b982a9
Corvu <Corvu@users.noreply.github.com>
2020-05-10 23:07:34 +0300

    Tabelite loomise fix

--- statements.sql
+++ statements.sql
@@ -18,48 +18,64 @@ CREATE TABLE kogudused (
    ,"nimi"                           varchar(45)
    ,"usutunnistus"                   varchar(45)
    ,"asutusaasta"                    integer
-   ,"kirik"                          integer
+   ,"kirik"                          integer NOT NULL
    ,"telefon"                        varchar(45)
-   ,PRIMARY KEY (id) 
+   ,PRIMARY KEY (id),
+   CONSTRAINT `fk_Kirikud_Kogudused`
+        FOREIGN KEY (`kirik`)
+        REFERENCES `Kirikud` (`id`),
 );
 
 CREATE TABLE organisatsioonid (
     "id"                             integer NOT NULL DEFAULT autoincrement
    ,"nimetus"                        varchar(45)
    ,"tüüp"                           varchar(45)
-   ,"kommentaarid"                   varchar(45)
    ,PRIMARY KEY (id) 
-);
-
-CREATE TABLE üritused (
-    "id"                             integer DEFAULT autoincrement
-   ,"nimetus"                        varchar(60)
-   ,"aeg"                            DATETIME
-   ,"tüüp"                           integer
-   ,"organiseerija"                  integer
-   ,"kirik"                          integer
-   ,"avatud"                         bit NOT NULL
-   ,"korduv"                         bit NOT NULL
-   ,"kommentaar"                     varchar(45)
-);
-
-CREATE TABLE isikud (
-    "id"                             integer NOT NULL
-   ,"nimi"                           varchar(45)
-   ,"isikukood"                      varchar(11)
-   ,PRIMARY KEY (id) 
-);
-
-CREATE TABLE osalemine (
-    "organisatsioonid_id"            integer
-   ,"isikud_id"                      integer
-   ,"id"                             integer
 );
 
 create table ürituste_tüübid (
     "id"                              integer NOT NULL DEFAULT autoincrement,
     "tüüp"                            varchar(30),
     PRIMARY KEY (id)
+);
+
+CREATE TABLE üritused (
+    "id"                             integer DEFAULT autoincrement,
+    "nimetus"                        varchar(60),
+    "aeg"                            DATETIME,
+    "tüüp"                           integer,
+    "organiseerija"                  integer,
+    "kirik"                          integer NOT NULL,
+    "avatud"                         bit NOT NULL,
+    "korduv"                         bit NOT NULL,
+    "kommentaar"                     varchar(45),
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_Üritused_Kirikud`
+        FOREIGN KEY (`kirik`)
+        REFERENCES `Kirikud` (`id`),
+    CONSTRAINT `fk_Üritused_Organisatsioonid`
+        FOREIGN KEY (`organiseerija`)
+        REFERENCES `Organisatsioonid` (`id`),
+    CONSTRAINT `fk_Üritused_Tüübid`
+        FOREIGN KEY (`tüüp`)
+        REFERENCES `Ürituste_Tüübid` (`id`))
+;
+
+CREATE TABLE isikud (
+    "isikukood"                      varchar(11),
+    "nimi"                           varchar(45),
+    PRIMARY KEY (isikukood) 
+);
+
+CREATE TABLE osalemine (
+    "organisatsioon"            integer,
+    "isik"                      VARCHAR(11),
+    CONSTRAINT "fk_Osalemine_Organisatsioon"
+        FOREIGN KEY ("organisatsioon")
+        REFERENCES "organisatsioonid" ("id"),
+    CONSTRAINT "fk_Osalemine_Isik"
+        FOREIGN KEY ("isik")
+        REFERENCES "isikud" ("isikukood")
 );
 
 
@@ -101,7 +117,7 @@ ALTER TABLE üritused
 -- Protseduurite loomine
 ----------------------------------------
 
---1. Tagastab kirikud, millel on etteantud arv torne
+-- 1. Tagastab kirikud, millel on etteantud arv torne
 create or replace procedure torniga_kirikud (in torni_arv integer)
 result (
     nimi varchar(50),
@@ -112,8 +128,7 @@ BEGIN
     where tornid = torni_arv
     order by torni_arv;
 end;
-// vastavalt sisendile nt: 0 - adventkirik 0/ 1 - Tartu katoliku kirik 1 Kolgata kirik 1 Jaani kirik 1/ 6 - Jumalaema uinumise katedraalkirik 6
-
+-- vastavalt sisendile nt: 0 - adventkirik 0/ 1 - Tartu katoliku kirik 1 Kolgata kirik 1 Jaani kirik 1/ 6 - Jumalaema uinumise katedraalkirik 6
 
 -- 2. Tagastab oreliga kirikud, kus peetakse jumalateenistust ja mis vastavad sisestatud usutunnistusele
 create or replace procedure oreliga_jumalat_kir (in i_usutunnistus varchar(50))
@@ -129,11 +144,7 @@ begin
     i_usutunnistus = kogudused.usutunnistus and
     kirikud.orel = 1;
 end;
-// vastavalt sisendile: 'Vene õigeusk': Jumalaema uinumise katedralkirik Vene õigeusk/ 'Evangeelne luterlus': Jaani kirik Evangeelne luterlus
-
-
-
-
+-- vastavalt sisendile: 'Vene õigeusk': Jumalaema uinumise katedralkirik Vene õigeusk/ 'Evangeelne luterlus': Jaani kirik Evangeelne luterlus
 
 -- 3. lisab ürituste tabelisse etteantud ürituse. Kui etteantud tüüpi pole ürituste tüüpide tabelis, siis protseduur loob tabelisse vastava tüübi
 create or replace procedure lisa_ür (in nimetus varchar(45), in aeg datetime, in ü_tüüp varchar(45), in organiseerija varchar(45), in kirik varchar(45), in avatud bit, in korduv bit, in kommentaar varchar(50), out i_id integer)
@@ -162,9 +173,7 @@ begin
 	select @@identity into i_id;
 	set a_id = i_id;
 end;
-//Protseduuri kutsumine ei anna midagi välja
-
-
+-- Protseduuri kutsumine ei anna midagi välja
 
 -- 4. Tagastab info kiriku ja koguduse kohta vastavalt etteantud usutunnistusele
 create or replace procedure kir_kog_inf (in i_usutunnistus varchar(50))
